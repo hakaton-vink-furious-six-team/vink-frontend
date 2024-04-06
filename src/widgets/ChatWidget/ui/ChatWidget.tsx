cf0b822340037865b68021602def0980b9d8b7a8
Lev Smirnov <levsmirnov2015@mail.ru>
2024-04-06 14:19:09 +0500

    Add adaptive

--- src/widgets/ChatWidget/ui/ChatWidget.tsx
+++ src/widgets/ChatWidget/ui/ChatWidget.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ResizableBox } from 'react-resizable';
 import styles from './ChatWidget.module.scss';
 import { Form } from '../../../features/Form';
@@ -29,18 +29,96 @@ function ChatWidget() {
   const isOpen = useAppSelector(selectIsOpen);
   const isFormSubmitted = useAppSelector(selectIsSubmitted);
 
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  const [minConstraints, setMinConstraints] = useState<[number, number]>([330, 400]);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+
+      if (window.innerWidth < 890) {
+        setMinConstraints([200, 300]);
+      } else {
+        setMinConstraints([330, 400]);
+      }
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const [windowWidth, setWindowWidth] = useState({
+    width: window.innerWidth,
+  });
+
+  const [width, setWidth] = useState<number>(330);
+
+  useEffect(() => {
+    function handleWidth() {
+      setWindowWidth({
+        width: window.innerWidth,
+      });
+
+      if (window.innerWidth < 890) {
+        setWidth(230);
+      } else {
+        setWidth(330);
+      }
+    }
+    window.addEventListener('resize', handleWidth);
+
+    handleWidth();
+
+    return () => window.removeEventListener('resize', handleWidth);
+  }, []);
+
+  const [windowHeight, setWindowHeight] = useState({
+    width: window.innerWidth,
+  });
+
+  const [height, setHeight] = useState<number>(400);
+
+  useEffect(() => {
+    function handleHeight() {
+      setWindowHeight({
+        width: window.innerWidth,
+      });
+
+      if (window.innerWidth < 890) {
+        setHeight(300);
+      } else {
+        setHeight(400);
+      }
+    }
+    window.addEventListener('resize', handleHeight);
+
+    handleHeight();
+
+    return () => window.removeEventListener('resize', handleHeight);
+  }, []);
+
   return (
     <div className={styles.widget}>
       <OpenChatButton />
       {isOpen && (
         <ResizableBox
           className={styles.window}
-          width={330}
-          height={400}
+          width={width}
+          height={height}
           resizeHandles={['nw']}
           handle={<span className={`${styles.custom_handle} ${styles.custom_handle_nw}`} />}
           handleSize={[8, 8]}
-          minConstraints={[330, 400]}
+          minConstraints={minConstraints}
           maxConstraints={[800, 550]}
         >
           <div className={styles.header}>
